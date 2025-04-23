7fc3082c8fc74e16188e0c800338d4580eb3118e
gonzalosovero13 <gsovero@pucp.edu.pe>
2025-04-23 02:54:50 -0500

    Add files via upload

---
+++ AutoservicioCinePUCPDA/src/main/java/pe/com/cinepucp/autoservicio/mysql/GeneroDAOImpl.java
@@ -0,0 +1,39 @@
+
+package pe.com.cinepucp.autoservicio.main.mysql;
+
+import java.util.List;
+import pe.com.cinepucp.autoservicio.dao.IGeneroDAO;
+import pe.com.cinepucp.autoservicio.model.peliculas.Genero;
+
+/**
+ *
+ * @author Computer
+ */
+public class GeneroDAOImpl implements IGeneroDAO{
+
+    @Override
+    public int insertar(Genero modelo) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean modificar(Genero modelo) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean eliminar(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Genero buscar(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<Genero> listar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ AutoservicioCinePUCPDA/src/main/java/pe/com/cinepucp/autoservicio/mysql/PeliculaDAOImpl.java
@@ -0,0 +1,39 @@
+
+package pe.com.cinepucp.autoservicio.main.mysql;
+
+import java.util.List;
+import pe.com.cinepucp.autoservicio.dao.IPeliculaDAO;
+import pe.com.cinepucp.autoservicio.model.peliculas.Pelicula;
+
+/**
+ *
+ * @author Computer
+ */
+public class PeliculaDAOImpl implements IPeliculaDAO{
+
+    @Override
+    public int insertar(Pelicula modelo) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean modificar(Pelicula modelo) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean eliminar(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Pelicula buscar(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<Pelicula> listar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
