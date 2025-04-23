64df4ecaad70765434e42bb4eeca4fe4bd714d57
gonzalosovero13 <gsovero@pucp.edu.pe>
2025-04-22 20:14:53 -0500

    Add files via upload

---
+++ AutoservicioCinePUCPDA/src/main/java/pe/com/cinepucp/autoservicio/dao/IGeneroDAO.java
@@ -0,0 +1,7 @@
+
+package pe.com.cinepucp.autoservicio.dao;
+import pe.com.cinepucp.autoservicio.model.peliculas.Genero;
+
+public interface IGeneroDAO extends ICrud<Genero>{
+    
+}
---
+++ AutoservicioCinePUCPDA/src/main/java/pe/com/cinepucp/autoservicio/dao/IPeliculaDAO.java
@@ -0,0 +1,7 @@
+
+package pe.com.cinepucp.autoservicio.dao;
+import  pe.com.cinepucp.autoservicio.model.peliculas.Pelicula;
+
+public interface IPeliculaDAO extends ICrud<Pelicula>{
+    
+}
