ecf6c742ba4bfa8477b801486384be92f318bfc3
gonzalosovero13 <gsovero@pucp.edu.pe>
2025-04-23 04:21:40 -0500

    Update PeliculaDAOImpl.java

--- AutoservicioCinePUCPDA/src/main/java/pe/com/cinepucp/autoservicio/mysql/PeliculaDAOImpl.java
+++ AutoservicioCinePUCPDA/src/main/java/pe/com/cinepucp/autoservicio/mysql/PeliculaDAOImpl.java
@@ -1,39 +1,81 @@
 
 package pe.com.cinepucp.autoservicio.main.mysql;
-
-import java.util.List;
+import java.sql.*;
 import pe.com.cinepucp.autoservicio.dao.IPeliculaDAO;
 import pe.com.cinepucp.autoservicio.model.peliculas.Pelicula;
+import pe.com.cinepucp.autoservicio.mysql.BaseDAOImpl;
 
 /**
- *
- * @author Computer
+   
  */
-public class PeliculaDAOImpl implements IPeliculaDAO{
+public class PeliculaDAOImpl extends BaseDAOImpl<Pelicula> implements IPeliculaDAO{
 
     @Override
-    public int insertar(Pelicula modelo) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    protected PreparedStatement comandoInsertar(Connection conn, Pelicula peli) throws SQLException {
+                // Usamos CALL para ejecutar el procedimiento almacenado
+        String sql = "{CALL sp_insertar_pelicula(?, ?, ?, ?)}";
+        CallableStatement stmt = conn.prepareCall(sql);
+        stmt.setString(1, peli.getTituloEs());
+        stmt.setString(2, peli.getTituloEn());
+        stmt.setInt(3, peli.getDuracionMin());
+        stmt.setString(4, peli.getClasificacion());
+        
+        return stmt;
     }
 
     @Override
-    public boolean modificar(Pelicula modelo) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    protected PreparedStatement comandoModificar(Connection conn, Pelicula peli) throws SQLException {
+        String sql = "{ CALL sp_actualizar_pelicula(?, ?, ?, ?, ?, ?, ?, ?, ?) }";
+        CallableStatement stmt = conn.prepareCall(sql);
+        stmt.setInt(1, peli.getId());
+        stmt.setString(2, peli.getTituloEs());
+        stmt.setString(3, peli.getTituloEn());
+        stmt.setInt(4, peli.getDuracionMin());
+        stmt.setString(5, peli.getClasificacion());
+        stmt.setString(6, peli.getSinopsisEn());
+        stmt.setString(7, peli.getSinopsisEn());
+        stmt.setString(8, peli.getImagenUrl());
+        stmt.setBoolean(9, peli.isEstaActiva());
+        return stmt;
     }
 
     @Override
-    public boolean eliminar(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    protected PreparedStatement comandoEliminar(Connection conn, int id) throws SQLException {
+        String sql = "{CALL sp_eliminar_pelicula(?)}";
+        CallableStatement stmt = conn.prepareCall(sql);
+        stmt.setInt(1, id);
+        return stmt;
     }
 
     @Override
-    public Pelicula buscar(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    protected PreparedStatement comandoBuscar(Connection conn, int id) throws SQLException {
+        String sql = "{CALL sp_buscar_pelicula(?)}";
+        CallableStatement stmt = conn.prepareCall(sql);
+        stmt.setInt(1, id);
+        return stmt;
     }
 
     @Override
-    public List<Pelicula> listar() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    protected PreparedStatement comandoListar(Connection conn) throws SQLException {
+        String sql = "{CALL sp_listar_peliculas()}";
+        return conn.prepareCall(sql);
     }
+
+    @Override
+    protected Pelicula mapearModelo(ResultSet rs) throws SQLException {
+        Pelicula peli = new Pelicula();
+        peli.setId(rs.getInt("pelicula_id"));
+        peli.setTituloEs(rs.getString("titulo_es"));
+        peli.setTituloEn(rs.getString("titulo_en"));
+        peli.setDuracionMin(rs.getInt("duracion_min"));
+        peli.setClasificacion(rs.getString("clasificacion"));
+        peli.setSinopsisEs(rs.getString("sinopsis_es"));
+        peli.setSinopsisEn(rs.getString("sinopsis_en"));
+        peli.setImagenUrl(rs.getString("imagen_url"));
+        peli.setEstaActiva(rs.getBoolean("esta_activa"));
+        return peli;
+    }
+
     
 }
+
