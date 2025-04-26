3e6f43141612d06c7ee1184f761a785f3e578e5b
Kayden32 <a20210678@pucp.edu.pe>
2025-04-26 15:19:26 -0500

    CRUD Cupón y Venta

---
+++ AutoservicioCinePUCPDA/src/main/java/pe/com/cinepucp/autoservicio/main/cruds/CuponCRUD.java
@@ -0,0 +1,88 @@
+package pe.com.cinepucp.autoservicio.main.cruds;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+import pe.com.cinepucp.autoservicio.model.auth.Usuario;
+import pe.com.cinepucp.autoservicio.model.venta.Cupon;
+import pe.com.cinepucp.autoservicio.mysql.CuponDAOImpl;
+
+public class CuponCRUD {
+
+    public static void ejecutarCRUDCupon(){
+        System.out.println("\n=== INICIO CRUD CUPON ===");
+        CuponDAOImpl cuponDAO = new CuponDAOImpl();
+
+        listarCupon("Cupones al inicio", cuponDAO);
+        
+        int idCupon = crearCuponEjm(cuponDAO);
+        buscarCupon(cuponDAO, idCupon);
+        actualizarCupon(cuponDAO, idCupon);
+        eliminarCupon(cuponDAO, idCupon);
+
+        listarCupon(null, cuponDAO);
+        System.out.println("\n=== CRUD CUPONES COMPLETADO CON ÉXITO ===");
+    }
+
+    private static int crearCuponEjm(CuponDAOImpl cuponDAO){
+        Cupon nuevoCupon = new Cupon();
+        nuevoCupon.setCodigo("DESCUENTO20");
+        nuevoCupon.setDescripcionEs("Descuento del 20% en tu compra");
+        nuevoCupon.setDescripcionEn("20% discount on your purchase");
+        nuevoCupon.setDescuentoTipo("PORCENTAJE");
+        nuevoCupon.setFechaInicio(LocalDate.now());
+        nuevoCupon.setFechaFin(LocalDate.now());
+        nuevoCupon.setMaxUsos(100);
+        nuevoCupon.setCreadoPor(new Usuario());
+        nuevoCupon.setDescuentoValor(new BigDecimal("0.20"));
+        
+        return insertarCupon(cuponDAO, nuevoCupon);
+    }
+
+    public static void listarCupon(String titulo, CuponDAOImpl cuponDAO){
+        System.out.println("\n" + titulo);
+        System.out.println("======================================");
+        List<Cupon> cupon = cuponDAO.listar();
+        if(cupon.isEmpty()){
+            System.out.println("No hay ventas registradas");
+        }else{
+            cupon.forEach(System.out::println);
+        }
+        System.out.println("======================================\n");
+
+    }
+
+    public static int insertarCupon(CuponDAOImpl cuponDAO, Cupon cupon){
+        int idCupon = cuponDAO.insertar(cupon);
+        if(idCupon == -1){
+            throw new RuntimeException("Error al crear el cupón");
+        }
+        System.out.println("CREATE: Cupón creado con ID: " + idCupon);
+        return idCupon;
+    }
+
+    public static Cupon buscarCupon(CuponDAOImpl cuponDAO, int id){
+        Cupon cupon = cuponDAO.buscar(id);
+        if (cupon == null) {
+            throw new RuntimeException("No se encontró el cupón con ID: " + id);
+        }
+        System.out.println("READ: Cupón encontrado - " + cupon);
+        return cupon;
+    }
+
+    public static void actualizarCupon(CuponDAOImpl cuponDAO,int id){
+        Cupon cupon = cuponDAO.buscar(id);
+        if (cupon == null) {
+            throw new RuntimeException("Error al actualizar el cupón con ID: " + id);
+        }
+        System.out.println("UPDATE: Cupón actualizado correctamente");
+    }
+
+    public static void eliminarCupon(CuponDAOImpl cuponDAO, int id) {
+        if (!cuponDAO.eliminar(id)) {
+            throw new RuntimeException("Error al eliminar el cupón con ID: " + id);
+        }
+        System.out.println("DELETE: Cupón eliminado correctamente");
+    }
+}
---
+++ AutoservicioCinePUCPDA/src/main/java/pe/com/cinepucp/autoservicio/main/cruds/VentaCRUD.java
@@ -0,0 +1,83 @@
+package pe.com.cinepucp.autoservicio.main.cruds;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import pe.com.cinepucp.autoservicio.model.venta.Venta;
+import pe.com.cinepucp.autoservicio.mysql.VentaDAOImpl;
+
+public class VentaCRUD {
+    public static void ejecutarCRUDVenta(){
+        System.out.println("\n=== INICIO CRUD VENTA ===");
+        VentaDAOImpl ventaDAO = new VentaDAOImpl();
+
+        listarVentas("Ventas al inicio", ventaDAO);
+        
+        int idVentas = crearVentaEjm(ventaDAO);
+        buscarVenta(ventaDAO, idVentas);
+        actualizarVenta(ventaDAO, idVentas);
+        eliminarVenta(ventaDAO, idVentas);
+
+        listarVentas(null, ventaDAO);
+        System.out.println("\n=== CRUD VENTAS COMPLETADO CON ÉXITO ===");
+    }
+
+    private static int crearVentaEjm(VentaDAOImpl ventaDAO){
+         Venta nuevaventa = new Venta();
+         nuevaventa.setSubtotal(new BigDecimal("100.00"));
+         nuevaventa.setImpuestos( new BigDecimal("15.00"));
+         nuevaventa.setFechaHora(LocalDateTime.now());
+         nuevaventa.setTotal();
+
+        return insertarVenta(ventaDAO,nuevaventa);
+
+    }
+
+    public static void listarVentas(String titulo, VentaDAOImpl ventaDAO){
+        System.out.println("\n" + titulo);
+        System.out.println("======================================");
+        List<Venta> ventas = ventaDAO.listar();
+        if(ventas.isEmpty()){
+            System.out.println("No hay ventas registradas");
+        }else{
+            ventas.forEach(System.out::println);
+        }
+        System.out.println("======================================\n");
+
+    }
+
+    public static int insertarVenta(VentaDAOImpl ventaDAO, Venta venta){
+        int idVenta = ventaDAO.insertar(venta);
+        if(idVenta == -1){
+            throw new RuntimeException("Error al crear la venta");
+        }
+        System.out.println("CREATE: Venta creada con ID: " + idVenta);
+        return idVenta;
+    }
+
+    public static Venta buscarVenta(VentaDAOImpl ventaDAO, int id){
+        Venta venta = ventaDAO.buscar(id);
+        if (venta == null) {
+            throw new RuntimeException("No se encontró la venta con ID: " + id);
+        }
+        System.out.println("READ: Venta encontrada - " + venta);
+        return venta;
+    }
+
+    public static void actualizarVenta(VentaDAOImpl ventaDAO,int id){
+        Venta venta = ventaDAO.buscar(id);
+        if (venta == null) {
+            throw new RuntimeException("Error al actualizar la venta con ID: " + id);
+        }
+        System.out.println("UPDATE: Venta actualizada correctamente");
+    }
+
+    public static void eliminarVenta(VentaDAOImpl ventaDAO, int id) {
+        if (!ventaDAO.eliminar(id)) {
+            throw new RuntimeException("Error al eliminar el venta con ID: " + id);
+        }
+        System.out.println("DELETE: venta eliminado correctamente");
+    }
+}
