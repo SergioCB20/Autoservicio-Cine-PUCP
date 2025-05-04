bce0f680a27f14b9161bf5fb6e11bc2f56ec5ee4
gonzalosovero13 <gsovero@pucp.edu.pe>
2025-05-03 20:05:10 -0500

    Update FuncionDAOImpl.java

--- AutoservicioCinePUCPDA/src/main/java/pe/com/cinepucp/autoservicio/mysql/FuncionDAOImpl.java
+++ AutoservicioCinePUCPDA/src/main/java/pe/com/cinepucp/autoservicio/mysql/FuncionDAOImpl.java
@@ -1,8 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package pe.com.cinepucp.autoservicio.mysql;
+
 import java.sql.*;
 import pe.com.cinepucp.autoservicio.dao.IFuncionDAO;
 import pe.com.cinepucp.autoservicio.model.Peliculas.Funcion;
@@ -13,39 +10,46 @@ import pe.com.cinepucp.autoservicio.model.salas.Sala;
  * @author Piero
  */
 public class FuncionDAOImpl extends BaseDAOImpl<Funcion> implements IFuncionDAO{
-    
+    private final int usuarioModificacionId = 1;
     @Override
     protected PreparedStatement comandoInsertar(Connection conn, Funcion funcion) throws SQLException {
-        String sql = "{CALL sp_insertar_funcion(?, ?, ?, ?, ?, ?)}";
+        String sql = "{CALL sp_insertar_funcion(?, ?, ?, ?, ?, ?, ?, ?, ?)}";
         CallableStatement stmt = conn.prepareCall(sql);
-        stmt.setInt(1, funcion.getSala().getId());
-        stmt.setTimestamp(2, Timestamp.valueOf(funcion.getFechaHora()));
-        stmt.setString(3, funcion.getFormatoProyeccion());
-        stmt.setString(4, funcion.getIdioma());
-        stmt.setBoolean(5, funcion.isSubtitulos());
-        stmt.setInt(6, funcion.getPelicula().getId());
+        
+        stmt.setInt(1, funcion.getPelicula().getPeliculaId());
+        stmt.setInt(2, funcion.getSala().getId());
+        stmt.setTimestamp(3, Timestamp.valueOf(funcion.getFechaHora()));
+        stmt.setString(4, funcion.getFormatoProyeccion());
+        stmt.setString(5, funcion.getIdioma());
+        stmt.setBoolean(6, funcion.getSubtitulos());
+        stmt.setBoolean(7, funcion.getEstaActiva());
+        stmt.setTimestamp(8, Timestamp.valueOf(funcion.getFechaModificacion()));
+        stmt.setInt(9, usuarioModificacionId);
         return stmt;
     }
     
     @Override
     protected PreparedStatement comandoModificar(Connection conn, Funcion funcion) throws SQLException {
-        String sql = "{CALL sp_actualizar_funcion(?, ?, ?, ?, ?, ?, ?)}";
+        String sql = "{CALL sp_actualizar_funcion(?, ?, ?, ?, ?, ?, ?, ?, ?)}";
         CallableStatement stmt = conn.prepareCall(sql);
-        stmt.setInt(1, funcion.getId());
-        stmt.setInt(2, funcion.getSala().getId());
-        stmt.setTimestamp(3, Timestamp.valueOf(funcion.getFechaHora()));
-        stmt.setString(4, funcion.getFormatoProyeccion());
-        stmt.setString(5, funcion.getIdioma());
-        stmt.setBoolean(6, funcion.isSubtitulos());
-        stmt.setInt(7, funcion.getPelicula().getId());
+        stmt.setInt(1, funcion.getFuncionId());
+        stmt.setInt(2, funcion.getPelicula().getPeliculaId());
+        stmt.setInt(3, funcion.getSala().getId());
+        stmt.setTimestamp(4, Timestamp.valueOf(funcion.getFechaHora()));
+        stmt.setString(5, funcion.getFormatoProyeccion());
+        stmt.setString(6, funcion.getIdioma());
+        stmt.setBoolean(7, funcion.getSubtitulos());
+        stmt.setBoolean(8, funcion.getEstaActiva());
+        stmt.setInt(9, usuarioModificacionId);
         return stmt;
     }
     
     @Override
     protected PreparedStatement comandoEliminar(Connection conn, int id) throws SQLException {
-        String sql = "{CALL sp_eliminar_funcion(?)}";
+        String sql = "{CALL sp_eliminar_funcion(?, ?)}";
         CallableStatement stmt = conn.prepareCall(sql);
         stmt.setInt(1, id);
+        stmt.setInt(2, usuarioModificacionId);
         return stmt;
     }
     
@@ -59,28 +63,27 @@ public class FuncionDAOImpl extends BaseDAOImpl<Funcion> implements IFuncionDAO{
     
     @Override
     protected PreparedStatement comandoListar(Connection conn) throws SQLException {
-        String sql = "{CALL sp_listar_funciones()}";
+        String sql = "{CALL sp_listar_funciones(FALSE)}";
         return conn.prepareCall(sql);
     }
     
     @Override
     protected Funcion mapearModelo(ResultSet rs) throws SQLException {
         Funcion funcion = new Funcion();
-        funcion.setId(rs.getInt("funcion_id"));
+        funcion.setFuncionId(rs.getInt("funcion_id"));
         
         // Mapear la sala asociada
         Sala sala = new Sala();
         sala.setId(rs.getInt("sala_id"));
-        sala.setNombre(rs.getString("sala_nombre"));
         funcion.setSala(sala);
         
         // Mapear la película asociada
         Pelicula pelicula = new Pelicula();
-        pelicula.setId(rs.getInt("pelicula_id"));
+        pelicula.setPeliculaId(rs.getInt("pelicula_id"));
         funcion.setPelicula(pelicula);
         
-        funcion.setFechaHora(rs.getTimestamp("fechaHora").toLocalDateTime());
-        funcion.setFormatoProyeccion(rs.getString("formatoProyeccion"));
+        funcion.setFechaHora(rs.getTimestamp("fecha_Hora").toLocalDateTime());
+        funcion.setFormatoProyeccion(rs.getString("formato_proyeccion"));
         funcion.setIdioma(rs.getString("idioma"));
         funcion.setSubtitulos(rs.getBoolean("subtitulos"));
         
