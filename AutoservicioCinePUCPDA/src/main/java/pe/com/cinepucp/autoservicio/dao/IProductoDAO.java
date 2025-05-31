5fbce8527df2b5dd97d9cb272103ec9ee5d2784c
gonzalosovero13 <gsovero@pucp.edu.pe>
2025-05-31 00:05:29 -0500

    Update IProductoDAO.java

--- AutoservicioCinePUCPDA/src/main/java/pe/com/cinepucp/autoservicio/dao/IProductoDAO.java
+++ AutoservicioCinePUCPDA/src/main/java/pe/com/cinepucp/autoservicio/dao/IProductoDAO.java
@@ -1,9 +1,10 @@
 package pe.com.cinepucp.autoservicio.dao;
+import pe.com.cinepucp.autoservicio.model.comida.Producto;
 
 /**
  *
  * @author User
  */
-public interface IProductoDAO {
+public interface IProductoDAO extends ICrud<Producto> {
     
 }
