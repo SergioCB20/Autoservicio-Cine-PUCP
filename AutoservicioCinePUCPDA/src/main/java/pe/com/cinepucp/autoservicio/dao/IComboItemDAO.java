83ad8d983d802919e7338c826f0f77807635105e
gonzalosovero13 <gsovero@pucp.edu.pe>
2025-05-31 00:07:22 -0500

    Add files via upload

---
+++ AutoservicioCinePUCPDA/src/main/java/pe/com/cinepucp/autoservicio/dao/IComboItemDAO.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.com.cinepucp.autoservicio.dao;
+
+import pe.com.cinepucp.autoservicio.model.comida.ComboItem;
+
+/**
+ *
+ * @author gonza
+ */
+public interface IComboItemDAO extends ICrud<ComboItem>{
+    
+}
