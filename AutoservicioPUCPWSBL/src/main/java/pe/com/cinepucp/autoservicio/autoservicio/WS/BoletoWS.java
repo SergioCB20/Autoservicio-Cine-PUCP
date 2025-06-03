2eb30afab1b7e109a10e1c56fdc761a2b8a5bf45
genele4n <a20210687@pucp.edu.pe>
2025-06-03 10:03:23 -0500

    BoletoWS

---
+++ AutoservicioPUCPWSBL/src/main/java/pe/com/cinepucp/autoservicio/autoservicio/WS/BoletoWS.java
@@ -0,0 +1,55 @@
+package pe.com.cinepucp.autoservicio.autoservicio.WS;
+
+import jakarta.jws.WebService;
+import jakarta.jws.WebMethod;
+import jakarta.jws.WebParam;
+import jakarta.xml.ws.WebServiceException;
+import java.util.List;
+import pe.com.cinepucp.autoservicio.autoserviciocinepucpbusiness.bo.IBoletoBO;
+import pe.com.cinepucp.autoservicio.autoserviciocinepucpbusiness.boimpl.BoletoBOImpl;
+import pe.com.cinepucp.autoservicio.model.venta.Boleto;
+
+/**
+ *
+ * @author Piero
+ */
+@WebService(serviceName = "BoletoWS")
+public class BoletoWS {
+
+    private final IBoletoBO boletoBO;
+    
+    public BoletoWS() {
+        boletoBO = new BoletoBOImpl();
+    }
+    
+    @WebMethod(operationName = "listarBoleto")
+    public List<Boleto> listar() {
+        return this.boletoBO.listar();
+    }
+    
+    @WebMethod(operationName = "actualizarBoleto")
+    public void guardarBoleto(@WebParam(name = "boleto")Boleto boleto) {
+        try {
+            this.boletoBO.actualizar(boleto);
+        } catch (Exception e) {
+            throw new WebServiceException("Error al guardar boleto: " + e.getMessage());
+        }
+    }
+    
+    @WebMethod(operationName = "obtenerBoleto")
+    public Boleto obtener(
+        @WebParam(name = "id") int id
+    ) {
+        return this.boletoBO.buscarPorId(id);
+    }
+    
+    @WebMethod(operationName = "eliminar")
+    public void eliminar(
+        @WebParam(name = "id") int id
+    ) {
+        this.boletoBO.eliminar(id);
+    }
+    
+    
+    
+}
